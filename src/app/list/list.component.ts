b64db9f90607b57c7248a8c8c21dd825b35a96c5
sundeep-co-in <suanand@redhat.com>
2018-04-17 20:07:09 +0530

    country filter

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -23,6 +23,38 @@ export class ListComponent implements OnInit {
   statusFilterLabel: string = this.statusFilterOptions[0];
   showFilterOptions = [];
 
+  alphabets = [];
+  countries = {
+    a: ['Argentina', 'Afganistan', 'Albania', 'Algeria', 'Andorra', 'Antigua and Barbuda', 'Argentina', 'Aruba', 'Australia'],
+    b: ['Bahamas', 'Belgium', 'Bermuda', 'Bhutan', 'Bolivia', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei'],
+    c: ['Cambodia', 'Canada', 'Central African Republic', 'Chile', 'China', 'Colombia', 'Cuba', 'Cyprus', 'Czech Republic'],
+    d: ['Democratic Republic of Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic'],
+    e: ['East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia'],
+    f: ['Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia'],
+    g: ['Georgia', 'Germany', 'Ghana', 'Greece', 'Greenland', 'Guatemala', 'Guinea', 'Guinea-Bissau'],
+    h: ['Haiti', 'Honduras', 'Hong Kong', 'Hungary'],
+    i: ['Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Ivory Coast'],
+    j: ['Jamaica', 'Japan', 'Jordan'],
+    k: ['Kazakhstan', 'Kenya', 'Kiribati', 'Kosovo', 'Kuwait', 'Kyrgyzstan'],
+    l: ['Laos', 'Latvia', 'Lebanon', 'Lesotho', '	Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg'],
+    m: ['Macau', 'Madagascar', 'Malaysia', 'Maldives', 'Marshall Islands', 'Mauritius', 'Mexico', 'Monaco', '	Myanmar'],
+    n: ['Nepal', 'Netherlands', 'New Zealand', 'Nigeria', 'North Korea', 'Norway'],
+    o: ['Oman'],
+    p: ['Pakistan', 'Panama', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico'],
+    q: ['Qatar'],
+    r: ['Republic of the Congo', 'Reunion', 'Romania', 'Russia', 'Rwanda'],
+    s: ['Saudi Arabia', 'Serbia', 'Singapore', 'Slovakia', 'South Africa', 'Spain', 'Sri Lanka', 'Switzerland', 'Syria'],
+    t: ['Taiwan', 'Thailand', 'Tokelau', 'Turkey', 'Tuvalu'],
+    u: ['Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', '	Uzbekistan'],
+    v: ['Vanuatu', 'Vatican', 'Venezuela', 'Vietnam'],
+    w: ['Western Sahara'],
+    x: [''],
+    y: ['Yemen'],
+    z: ['Zambia', 'Zimbabwe'],
+  };
+
+  countrySelectCount: number = 0;
+
   constructor(
     private userService: UserDataService,
     private dialog: MatDialog
@@ -66,15 +98,47 @@ export class ListComponent implements OnInit {
     this.dataSource.filter = filterValue;
   }
 
+  showStatusFilters() {
+    if (this.showFilterOptions.length > 0) {
+      this.showFilterOptions = [];
+    }
+    if (this.alphabets.length > 0) {
+      this.alphabets = [];
+    }
+    this.showFilterOptions.push.apply(this.showFilterOptions, this.statusFilterOptions);
+  }
+
   applyStatusFilter(filterValue: string) {
     if (filterValue == this.statusFilterOptions[1]) {
       this.dataSource.filter = "true";
+      this.statusFilterLabel = filterValue;
     } else if (filterValue == this.statusFilterOptions[2]) {
       this.dataSource.filter = "false";
-    } else {
+      this.statusFilterLabel = filterValue;
+    } else if (filterValue == this.statusFilterOptions[0]) {
       this.dataSource.filter = "";
+      this.statusFilterLabel = filterValue;
+    } else {
+      this.applyFilter(filterValue);
     }
-    this.statusFilterLabel = filterValue;
+    
+  }
+
+  showAlphabets(){
+    if (this.showFilterOptions.length > 0) {
+      this.showFilterOptions = [];
+    }
+    this.alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
+  }
+
+  showCountries(alphabet: string) {
+    if (this.showFilterOptions.length > 0) {
+      this.showFilterOptions = [];
+    }
+    this.showFilterOptions.push.apply(
+      this.showFilterOptions, 
+      this.countries[alphabet].sort()
+    );
   }
 
   resetFilters() {
@@ -82,17 +146,13 @@ export class ListComponent implements OnInit {
     // reset status filter
     this.showFilterOptions = [];
     this.statusFilterLabel = this.statusFilterOptions[0];
+    // reset country filter
+    this.alphabets = [];
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-  }
-
-  showStatusFilters() {
-    if (this.showFilterOptions.length != this.statusFilterOptions.length) {
-      this.showFilterOptions.push.apply(this.showFilterOptions, this.statusFilterOptions);
-    }
   }
 
 }
--- todo.md
+++ todo.md
@@ -1,7 +1,13 @@
 ### ToDo / Improvements List
 
  * `Invalid Email/Password` error message
+    * for unsuccessful login
+ * Country filter custom component
+    * Plus, multicountry select filter
  * More polished UI components
     * maybe bootstrap navbar and datatable
+    * side active tab style
+    * settings in navbar
+    * filters card and arrangements
  * Mobile Responsiveness
- *
+ * Unit test cases in jasmine
